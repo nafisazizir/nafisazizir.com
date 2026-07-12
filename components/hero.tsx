96a753aa98b50675085c389f9a5192bd8e6d32d2
nafisaziziriza <nafisaziziriza@gmail.com>
2026-07-12 16:30:33 +1000

    style: normalize quotes and semicolons in hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import { GradientShader } from "@/components/shaders/gradient-shader";
+import { GradientShader } from "@/components/shaders/gradient-shader"
 
 export function Hero() {
   return (
     <section className="relative h-dvh w-full overflow-hidden bg-background">
       <GradientShader className="absolute inset-0" />
     </section>
-  );
+  )
 }
