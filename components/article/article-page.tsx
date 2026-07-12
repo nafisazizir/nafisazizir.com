349a96a95429c4f3205b3c383df6c1ce9c6b2939
nafisaziziriza <nafisaziziriza@gmail.com>
2026-07-12 20:50:49 +1000

    feat: add title

--- components/article/article-page.tsx
+++ components/article/article-page.tsx
@@ -1,6 +1,7 @@
 import { MDXContent } from "@/components/article/mdx-content"
 import { Toc } from "@/components/article/toc"
 import type { Post } from "@/lib/content"
+import { formatDate } from "@/lib/utils"
 
 export function ArticlePage({ post }: { post: Post }) {
   return (
@@ -9,6 +10,17 @@ export function ArticlePage({ post }: { post: Post }) {
         <Toc items={post.toc} />
         <main className="mx-auto max-w-160">
           <article className="typeset typeset-article">
+            <header className="mb-10">
+              <h1 style={{ marginBlockStart: 0 }}>
+                {post.frontmatter.title}
+              </h1>
+              <time
+                dateTime={post.frontmatter.date}
+                className="text-muted-foreground text-sm"
+              >
+                {formatDate(post.frontmatter.date)}
+              </time>
+            </header>
             <MDXContent source={post.content} />
           </article>
         </main>
